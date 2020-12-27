a0f667460b4cac1d9acfc901cf1014d9c8ad9cc8
Florian Fontan <dev@florian-fontan.fr>
2020-12-27 11:01:39 +0100

    Update .ycm_extra_conf.py

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -7,6 +7,7 @@ def Settings( **kwargs ):
                 '-I', '.',
                 '-I', './bazel-stablesolver/external/json/single_include',
                 '-I', './bazel-stablesolver/external/googletest/googletest/include',
+                '-I', './bazel-stablesolver/external/boost/',
                 '-I', './bazel-stablesolver/external/optimizationtools',
                 '-I', '/opt/ibm/ILOG/CPLEX_Studio129/concert/include/',
                 '-I', '/opt/ibm/ILOG/CPLEX_Studio129/cplex/include/',
