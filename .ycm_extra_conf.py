8b935b32ab973679fbae0a9b794d98622a42f1d7
Florian Fontan <dev@florian-fontan.fr>
2023-01-08 11:47:24 +0100

    Update WORKSPACE

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -1,24 +1,39 @@
-def Settings( **kwargs ):
+def Settings(**kwargs):
     return {
             'flags': [
                 '-x', 'c++',
                 '-Wall', '-Wextra', '-Werror',
-                '-DCPLEX_FOUND',
-                '-DCOINOR_FOUND',
-                '-DIL_STD', # Cplex
                 '-I', '.',
-                '-I', './bazel-stablesolver/external/json/single_include',
-                '-I', './bazel-stablesolver/external/googletest/googletest/include',
-                '-I', './bazel-stablesolver/external/boost/',
-                '-I', './bazel-stablesolver/external/optimizationtools',
-                # '-I', '../optimizationtools',
-                '-I', './bazel-stablesolver/external/localsearchsolver/',
+
+                '-I', './bazel-stablesolver/external/'
+                'json/single_include/',
+
+                '-I', './bazel-stablesolver/external/'
+                'googletest/googletest/include/',
+
+                '-I', './bazel-stablesolver/external/'
+                'boost/',
+
+                # optimizationtools
+                '-I', './bazel-stablesolver/external/'
+                # '-I', './../'
+                'optimizationtools/',
+
+                # localsearchsolver
+                '-I', './bazel-stablesolver/external/'
+                # '-I', './../',
+                'localsearchsolver/',
+
                 # COINOR
+                '-DCOINOR_FOUND',
                 '-I', '/home/florian/Programmes/coinbrew/dist/include/',
+
                 # CPLEX
+                '-DCPLEX_FOUND',
+                '-DIL_STD',
                 '-I', '/opt/ibm/ILOG/CPLEX_Studio129/concert/include/',
                 '-I', '/opt/ibm/ILOG/CPLEX_Studio129/cplex/include/',
                 '-I', '/opt/ibm/ILOG/CPLEX_Studio129/cpoptimizer/include/',
+
                 ],
             }
-
