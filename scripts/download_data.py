48fc98db7408ef087dbdac77cd5e6ec1f595e903
Florian Fontan <dev@florian-fontan.fr>
2024-07-14 15:10:37 +0200

    Update download script

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -13,8 +13,18 @@ parser.add_argument(
         help='')
 args = parser.parse_args()
 
+def download(id):
+    for _ in range(3):
+        try:
+            gdown.download(id=id, output="data.7z")
+        except:
+            time.sleep(10)
+            continue
+        return
+    sys.exit(1)
+
 if args.data is None:
-    gdown.download(id="1OUJqeX9IMlRczq4Ycta-lppcnGgFJd2E", output="data.7z")
+    download("1OUJqeX9IMlRczq4Ycta-lppcnGgFJd2E")
     os.system("7z x data.7z")
     pathlib.Path("data.7z").unlink()
     shutil.copytree("stable", "data", dirs_exist_ok=True)
